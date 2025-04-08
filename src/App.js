dbc4f171a5f137ffe2f15bfa1b7111d7effa8bec
Rudra356 <rudranaskar66@gmail.com>
2025-04-08 12:18:01 +0530

    Disabled broswer shortcuts and inspections

--- src/App.js
+++ src/App.js
@@ -8,41 +8,41 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import 'font-awesome/css/font-awesome.min.css';
 
 function App() {
-  // useEffect(() => {
+  useEffect(() => {
     
-  //   const disableRightClick = (event) => event.preventDefault();
-  //   document.addEventListener("contextmenu", disableRightClick);
+    const disableRightClick = (event) => event.preventDefault();
+    document.addEventListener("contextmenu", disableRightClick);
 
     
-  //   const disableInspectElement = (event) => {
-  //     if (
-  //       event.key === "F12" ||
-  //       (event.ctrlKey &&
-  //         (event.key === "u" ||
-  //           event.key === "U" ||
-  //           event.key === "i" ||
-  //           event.key === "I" ||
-  //           event.key === "j" ||
-  //           event.key === "J" ||
-  //           event.key === "s" ||
+    const disableInspectElement = (event) => {
+      if (
+        event.key === "F12" ||
+        (event.ctrlKey &&
+          (event.key === "u" ||
+            event.key === "U" ||
+            event.key === "i" ||
+            event.key === "I" ||
+            event.key === "j" ||
+            event.key === "J" ||
+            event.key === "s" ||
 
-  //           event.key === "c" ||
-  //           event.key === "C" ||
-  //           event.key === "v" ||
-  //           event.key === "V" ||
-  //           event.key === "S"))
-  //     ) {
-  //       event.preventDefault();
-  //     }
-  //   };
-  //   document.addEventListener("keydown", disableInspectElement);
+            event.key === "c" ||
+            event.key === "C" ||
+            event.key === "v" ||
+            event.key === "V" ||
+            event.key === "S"))
+      ) {
+        event.preventDefault();
+      }
+    };
+    document.addEventListener("keydown", disableInspectElement);
 
-  //   // Cleanup Event Listeners on Component Unmount
-  //   return () => {
-  //     document.removeEventListener("contextmenu", disableRightClick);
-  //     document.removeEventListener("keydown", disableInspectElement);
-  //   };
-  // }, []);
+    // Cleanup Event Listeners on Component Unmount
+    return () => {
+      document.removeEventListener("contextmenu", disableRightClick);
+      document.removeEventListener("keydown", disableInspectElement);
+    };
+  }, []);
 
   return (
     <>
