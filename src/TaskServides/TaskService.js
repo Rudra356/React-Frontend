721bf63e860283c36daee264ce50db1ac0e61b53
Rudra356 <rudranaskar66@gmail.com>
2025-04-08 12:08:37 +0530

    Passing crenditials with axios request

--- src/TaskServides/TaskService.js
+++ src/TaskServides/TaskService.js
@@ -1,18 +1,26 @@
 import axios from "axios";
+
 const BASE_URL = "http://localhost:8080/Tasks";
-class  TaskService{
-    saveTask(task){
-        return axios.post(BASE_URL,task);
+
+class TaskService {
+    saveTask(task) {
+        return axios.post(BASE_URL, task, {
+            withCredentials: true,
+        });
     }
-    getAllTask(){
+
+    getAllTask() {
         console.log("Getting");
-        console.log(axios.get(BASE_URL));
-        return axios.get(BASE_URL);
+        return axios.get(BASE_URL, {
+            withCredentials: true,
+        });
     }
-   
-    }
-    // updateTaskById(task, taskId){
-    //     return axios.put(BASE_URL+"/"+taskId,task);
+
+    // updateTaskById(task, taskId) {
+    //     return axios.put(`${BASE_URL}/${taskId}`, task, {
+    //         withCredentials: true,
+    //     });
     // }
+}
 
 export default new TaskService();
